2e204d9cf1ff49e3712a9dae4b8dba6add584577
Brandon Simpson <brandonsimpson21@gmail.com>
2023-12-23 13:54:51 -0700

    more error conversions

--- src/error.rs
+++ src/error.rs
@@ -1,19 +1,28 @@
+use std::{array::TryFromSliceError, env::VarError, io};
 use thiserror::Error;
-use std::{io, env::VarError};
 
 #[derive(Error, Debug)]
 pub enum PlaygroundError {
     #[error("io error")]
     IoError(#[from] io::Error),
+    #[error("Try from slice error")]
+    TryFromSliceError(#[from] TryFromSliceError),
+    #[error("Network error {0}")]
+    NetworkError(String),
+    #[error("rustls error {0}")]
+    RustlsError(#[from] rustls::Error),
+    #[error("join error")]
+    JoinError(#[from] tokio::task::JoinError),
     #[error("Internal Error {0}")]
     InternalError(String),
-    #[error("unknown data store error")]
+    #[error("Var Error")]
+    VarError(#[from] VarError),
+    #[error("unknown error")]
     Unknown,
 }
 
-
-impl From<VarError> for PlaygroundError {
-    fn from(error: VarError) -> Self {
-        PlaygroundError::InternalError(format!("VarError: {}", error))
+impl From<Box<dyn std::error::Error>> for PlaygroundError {
+    fn from(error: Box<dyn std::error::Error>) -> Self {
+        PlaygroundError::InternalError(format!("Boxed Error: {}", error))
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,4 @@
 pub mod error;
-pub mod tracing;
 
 #[cfg(test)]
 mod tests {
